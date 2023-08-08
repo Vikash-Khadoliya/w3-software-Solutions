9319795ab6ff903b315d364fe5e2b2f8c3c56099
christiannaira <christianmnaira@gmail.com>
2023-08-08 15:30:43 +0630

    installed bootstrap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,31 @@ export default function Hero() {
     return (
         <>
 
+            <section className="hero " id="hero">
+
+                <div className="hero-content border row">
+
+
+
+                    <div className="hero-txt col-md-6">
+                        <h1>Better Solutions For Your Business</h1>
+                        <p>We are team of talented designers making websites with Bootstrap</p>
+
+                        <div className="hero-btn">
+
+                        </div>
+
+                    </div>
+
+                    <div className="hero-img col-md-6">
+                        asd
+                    </div>
+
+                </div>
+
+
+            </section>
+
         </>
     )
 
