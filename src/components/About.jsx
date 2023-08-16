24e19b7a3a8e2e7d23ba339797c039df42da9901
christiannaira <christianmnaira@gmail.com>
2023-08-16 10:54:43 +0630

    fixed the responsivess of all sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
 
             <section className="about" id="about">
 
-                <div className="about-content">
+                <div className="about-content ">
 
                     <div className="main">
                         <h2>ABOUT US</h2>
@@ -15,7 +15,7 @@ export default function About() {
 
                     <div className="about-txt-content row">
 
-                        <div className="about-txt col-lg-6">
+                        <div className="about-txt col-md-6">
 
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
@@ -27,7 +27,7 @@ export default function About() {
 
                         </div>
 
-                        <div className="about-btn col-lg-6">
+                        <div className="about-btn col-md-6">
                             <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             <a href="">Learn More</a>
                         </div>
--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -7,6 +7,14 @@ import ClientSix from '../assets/client-6.png';
 
 export default function Partners() {
 
+    // window.addEventListener('resize', function () {
+
+    //     if (window.innerWidth <= 615) {
+    //         console.log("hello");
+    //     }
+
+    // })
+
     return (
         <>
 
@@ -17,15 +25,18 @@ export default function Partners() {
                     <div className="col-lg-6">
 
                         <div className="row">
-                            <div className="partner-logo col-sm-4 ">
+
+                            <div className="partner-logo col-4 ">
+
                                 <img src={ClientOne} alt="" />
+
                             </div>
 
-                            <div className="partner-logo col-sm-4">
+                            <div className="partner-logo col-4">
                                 <img src={ClientTwo} alt="" />
                             </div>
 
-                            <div className="partner-logo col-sm-4">
+                            <div className="partner-logo col-4">
                                 <img src={ClientThree} alt="" />
                             </div>
                         </div>
@@ -36,15 +47,15 @@ export default function Partners() {
                     <div className="col-lg-6">
 
                         <div className="row">
-                            <div className="partner-logo col-sm-4">
+                            <div className="partner-logo col-4">
                                 <img src={ClientFour} alt="" />
                             </div>
 
-                            <div className="partner-logo col-sm-4">
+                            <div className="partner-logo col-4">
                                 <img src={ClientFive} alt="" />
                             </div>
 
-                            <div className="partner-logo col-sm-4">
+                            <div className="partner-logo col-4">
                                 <img src={ClientSix} alt="" />
                             </div>
                         </div>
